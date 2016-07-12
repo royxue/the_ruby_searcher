ec2cb5bad16360779c3614b40c7861247ef32d7c
Roy Xue <xljroy@gmail.com>
2016-07-12 14:05:58 -0700

    Basic content

---
+++ lib/the_ruby_searcher.rb
@@ -0,0 +1,32 @@
+require 'open3'
+
+class Rbsearcher
+  def initialize(engine='ag')
+  	@engine = engine
+  end
+
+  def check
+  	cmd = ["which", @engine].join(" ")
+    stdout, stdeerr, status = Open3.capture3(cmd)
+    if stdout.include? 'not found'
+      return false
+    else
+      return true
+    end
+  end
+
+  def switch(engine)
+    @engine = engine
+  end
+
+  def search(pattern, path)
+  	if not check()
+  	  puts "Select Engine not exist, automatically switch to grep"
+      @engine = 'grep'
+    end
+  	cmd = [@engine, pattern, path].join(" ")
+  	puts cmd
+  	stdout, stdeerr, status = Open3.capture3(cmd)
+  	return stdout, stdeerr, status
+  end
+end
---
+++ the_ruby_searcher.gemspec
@@ -0,0 +1,14 @@
+Gem::Specification.new do |s|
+  s.name        = "the_ruby_searcher"
+  s.version     = '0.0.1'
+  s.authors     = ["Roy Xue"]
+  s.email       = "lxue@tesla.com"
+  s.homepage    = "https://github.com/royxue/the_ruby_searcher"
+  s.summary     = "A Ruby Searcher"
+  s.description = "A Ruby Searcher"
+  s.required_rubygems_version = ">= 1.3.6"
+  s.files = ["lib/the_ruby_searcher.rb"]
+  # s.add_dependency 'some-gem'
+  # s.extra_rdoc_files = ['README.md', 'LICENSE']
+  # s.license = 'MIT'
+end
